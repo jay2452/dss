7907ff373ec2f1e8e1e85ced66dd28f39184d876
jay2452 <jay2452@live.com>
2016-06-23 20:48:52 +0530

    layouts and logo

--- config/routes.rb
+++ config/routes.rb
@@ -16,12 +16,15 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   #  root 'devise/sessions#new'
   devise_scope :user do
+    get '/login' => 'devise/sessions#new', as: :login
     unauthenticated do
       root 'devise/sessions#new'
     end
