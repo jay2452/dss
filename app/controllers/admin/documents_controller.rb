35503618bb56c690ae0998c0d5d5c5fec3a1c1a6
jay2452 <jay2452@live.com>
2016-09-28 20:52:26 +0530

    views admiin documents

--- app/controllers/admin/documents_controller.rb
+++ app/controllers/admin/documents_controller.rb
@@ -8,7 +8,6 @@ module Admin
     # GET /documents.json
     def index
       @documents = Document.all.order(group_id: :desc, created_at: :desc)
-
       # @approved_docs = Document.where('approved = ?', true).order(created_at: :desc)
       # @unApproved_docs = Document.where('approved = ?', false).order(created_at: :desc)
     end
@@ -31,11 +30,12 @@ module Admin
     # POST /documents.json
     def create
       @document = Document.new(document_params)
+      @document.user_id = current_user.id
 
       respond_to do |format|
         if @document.save
           Log.create! description: "#{current_user.email} uploaded #{@document.name} at #{@document.created_at}"
-          format.html { redirect_to @document, notice: 'Document was successfully created.' }
+          format.html { redirect_to admin_document_path(@document), notice: 'Document was successfully created.' }
           format.json { render :show, status: :created, location: @document }
         else
           format.html { render :new }
@@ -50,7 +50,7 @@ module Admin
       respond_to do |format|
         if @document.update(document_params)
           Log.create! description: "#{current_user.email} updadted #{@document.name} at #{@document.updated_at}"
-          format.html { redirect_to @document, notice: 'Document was successfully updated.' }
+          format.html { redirect_to admin_document_path(@document), notice: 'Document was successfully updated.' }
           format.json { render :show, status: :ok, location: @document }
         else
           format.html { render :edit }
@@ -78,7 +78,7 @@ module Admin
 
       # Never trust parameters from the scary internet, only allow the white list through.
       def document_params
-        params.require(:document).permit(:name, :document_category_id)
+        params.require(:document).permit(:name, :group_id, :file, :user_id)
       end
   end
 
--- app/models/document.rb
+++ app/models/document.rb
@@ -21,5 +21,5 @@ class Document < ActiveRecord::Base
 
   has_attached_file :file
 	validates_attachment :file, size: { in: 0..100.megabytes }
-	validates_attachment_content_type :file, content_type: %w(application/pdf)
+	validates_attachment_content_type :file, content_type: %w(application/pdf image/jpg image/jpeg image/png image/gif)
 end
