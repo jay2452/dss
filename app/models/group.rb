26b17abd94b54361a0d3a4dcc76e184bcc5db6a0
jay2452 <jay2452@live.com>
2016-09-28 16:58:10 +0530

    groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,8 +3,8 @@ class Group < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  has_many :document_groups
-  has_many :documents#, through: :document_groups
+  has_many :document_groups, dependent: :destroy
+  has_many :documents, dependent: :destroy#, through: :document_groups
 
   has_many :user_groups, dependent: :destroy
   has_many :users, through: :user_groups
