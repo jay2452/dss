853d0c03a3e10e9ee462b7a02cd4691b740a5028
jay2452 <jay2452@live.com>
2016-10-21 20:44:28 +0530

    infinite scroll on view user home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,22 +11,15 @@ class WelcomeController < ApplicationController
 
 
     @user_groups = current_user.groups#.where("group_id != ?", Group.first.id) # => - Group.first # => first group will contan the list of all users
-
     # @user_groups_1 = current_user.groups
-
     arr = []
     #   fetch all the id of documents from groups in which the user belongs to.
-
     if !@user_groups.empty?
       @user_groups.each do |group|
         group.documents.where("documents.deleted = ?", false).each do |doc| # => it is fetching records from DocumentGroup table
           arr << doc.id
         end
       end
-      #
-      # puts "++++++++++++++"
-      #   p arr
-      # puts "++++++++++++++"
     end
 
     # current_user.documents.each do |doc|
@@ -35,7 +28,7 @@ class WelcomeController < ApplicationController
 
     # arr.uniq!
     # => recent docs only for the viewuser purpose to veiw the files uploaded recently in his project/group
-    @recent_docs = DocumentGroup.where("document_id IN (?)", arr).order(created_at: :desc)
+    @recent_docs = DocumentGroup.where("document_id IN (?)", arr).paginate(:page => params[:page], :per_page => 10).order(created_at: :desc)
 
 
     #  fetch documents from Document Groups table
